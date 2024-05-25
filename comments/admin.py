ab0fc25de6d94d3e611d7b7dcec52009540da8fe
ilmtvv <napalkah@gmail.com>
2024-05-25 21:36:05 +0300

    add_admin_panel

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from comments.models import Comment
 
-# Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'created_at', 'updated_at')
+    search_fields = ('author__username', 'post__title')
